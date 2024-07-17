73e1c06089221c524a57cd9f8d3bd227270a86a5
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2024-07-17 23:03:00 +0530

    Update app.py

--- RCLogger/Source/app.py
+++ RCLogger/Source/app.py
@@ -29,6 +29,8 @@ st.markdown(
 )
 image = Image.open("RCLogger/Source/RCLogo.png")
 col1,col2,col3 = st.columns([1,1,1])
+with col1:
+    st.markdown("<h1 class='title'></h1>", unsafe_allow_html=True)
 with col2:
     st.image(image, output_format='auto',width=100)
     
