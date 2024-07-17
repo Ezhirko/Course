e3e167e27048ebb9c37a333f4c2ffc11954254c9
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2024-07-17 22:25:01 +0530

    Update app.py

--- RCLogger/Source/app.py
+++ RCLogger/Source/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 
-with open("Source\styles.css") as f:
+with open("styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 col1, col2 = st.columns([1, 3])
