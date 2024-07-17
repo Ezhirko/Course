e49cb620ee9a7409f8cbaf5b088193372e8fb3c9
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2024-07-17 22:31:42 +0530

    Update app.py

--- RCLogger/Source/app.py
+++ RCLogger/Source/app.py
@@ -25,11 +25,5 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-col1, col2 = st.columns([1, 3])
-
-with col1:
-    st.image("Resources\RCLogo.png", width=150)
-with col2:
-    st.markdown("<h1 class='title'>RADIANT CHAMPS</h1>", unsafe_allow_html=True)
-
+st.markdown("<h1 class='title'>RADIANT CHAMPS</h1>", unsafe_allow_html=True)
 st.markdown("<h1 class='subheading'>Badminton Academy</h1>", unsafe_allow_html=True)
